dc28cefdee5a35abcb2be0398c85a7b6f74ab86b
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2020-04-24 03:17:27 +0530

    added context API to share state between components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "./context";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>App component</h1>
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contacts/>
+      <Provider>
+        <div className="App">
+          <h1>App component</h1>
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,49 +1,25 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "+9187345349854",
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "KarenSmith@gmail.com",
-        phone: "+9184565349854",
-      },
-      {
-        id: 3,
-        name: "Shawn Parker",
-        email: "ShawnParker@gmail.com",
-        phone: "+9180345349854",
-      },
-    ],
-  };
-
-  deleteContact = (id) => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter((c) => c.id !== id);
-    this.setState({ contacts: newContacts });
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map((contact) => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {(value) => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map((contact) => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                 />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
