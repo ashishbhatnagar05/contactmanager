d90d76be8e0b020b29102144e1f87ac05f44c254
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2020-04-24 02:31:43 +0530

    added icon and onClick event

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true,
+  };
+  onShowClick = (e) => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">{email}</li>
           <li className="list-group-item">{phone}</li>
@@ -17,9 +27,7 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
+  contact: PropTypes.object.isRequired,
 };
 
 export default Contact;
