75f9ffe0969d0156dbe32d0765cd00a880068090
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2020-04-23 14:46:47 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import Contact from './components/Contact'
-import Header from './components/Header'
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>App component</h1>
-        <Header branding='Contact Manager'/>
-        <Contact name='John Doe' email='johnDoe@gmail.com' phone='+918745389201'/>
-        <Contact name='Karen Smith' email='KarenSmith@gmail.com' phone='+919604534235'/>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="johnDoe@gmail.com"
+            phone="+918745389201"
+          />
+          <Contact
+            name="Karen Smith"
+            email="KarenSmith@gmail.com"
+            phone="+919604534235"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-
- const Header=(props)=> {
-     const {branding}=props;
-    return (
+const Header = (props) => {
+  const { branding } = props;
+  return (
+    <nav className="navbar navbar-expand-sm.navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
         <div>
-            <h1 style={headingStyle}>{branding}</h1>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
         </div>
-    )
-}
-
-Header.defaulProps={
-    branding:'MyApp'
+      </div>
+    </nav>
+  );
 };
 
-Header.propTypes={
-    branding:PropTypes.string.isRequired
-}
-const headingStyle={color:'red',fontSize:'50px'};
+Header.defaulProps = {
+  branding: "MyApp",
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired,
+};
 export default Header;
