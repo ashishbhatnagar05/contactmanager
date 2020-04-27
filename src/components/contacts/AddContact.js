967322d7c4cdc83db68f8d764bf15287517267c4
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2020-04-27 09:19:15 +0530

    Added errors and error handling

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
 import { v4 as uuidv4 } from "uuid";
+import TextInputGroup from "../layout/TextInputGroup";
 
 class AddContact extends Component {
   state = {
     name: "",
     email: "",
     phone: "",
+    errors: {},
   };
 
-  onContactChange = (e) =>
+  onChange = (e) =>
     this.setState({
       [e.target.name]: [e.target.value],
     });
@@ -17,6 +19,19 @@ class AddContact extends Component {
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
+      return;
+    }
+    if (email === "") {
+      this.setState({ errors: { email: "Email is required" } });
+      return;
+    }
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is required" } });
+      return;
+    }
+
     const newContact = {
       id: uuidv4(),
       name,
@@ -28,11 +43,12 @@ class AddContact extends Component {
       name: "",
       email: "",
       phone: "",
+      errors: {},
     });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
     return (
       <Consumer>
         {(value) => {
@@ -42,39 +58,34 @@ class AddContact extends Component {
               <div className="card-header">Add COntact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      className="form-control form-control-lg"
-                      placeholder="Enter name......"
-                      value={name}
-                      onChange={this.onContactChange}
-                    ></input>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="name">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Email......"
-                      value={email}
-                      onChange={this.onContactChange}
-                    ></input>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="name">Phone</label>
-                    <input
-                      type="text"
-                      name="phone"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Phone......"
-                      value={phone}
-                      onChange={this.onContactChange}
-                    ></input>
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  ></TextInputGroup>
+
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email"
+                    type="email"
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  ></TextInputGroup>
+
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone"
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  ></TextInputGroup>
+
                   <input
                     type="submit"
                     value="Add Contact"
